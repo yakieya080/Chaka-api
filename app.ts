f9ef1ca3d80d7ec21e63dfba90608f2d21252de9
yakieya080 <yakieya080@gmail.com>
2026-06-08 07:21:18 +0300

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 import express, { type Express } from "express";
 import cors from "cors";
 import pinoHttp from "pino-http";
-import router from "./index.ts";
+"./index.ts";
 import { logger } from "./lib/logger";
 
 const app: Express = express();
