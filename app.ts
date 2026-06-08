905fcec6259653bd1471b0b322137820ab2b8aab
yakieya080 <yakieya080@gmail.com>
2026-06-08 12:59:04 +0300

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,10 +1,14 @@
 import express, { type Express } from "express";
 import cors from "cors";
 import pinoHttp from "pino-http";
+import router from "./routes";
+import { logger } from "./lib/logger";
+
 const app: Express = express();
 
 app.use(
   pinoHttp({
+    logger,
     serializers: {
       req(req) {
         return {
@@ -25,5 +29,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/api", router);
 
 export default app;
